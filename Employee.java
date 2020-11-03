7c631760c969e61f56c05408284730fe11f68932
Amit366 <60662775+Amit366@users.noreply.github.com>
2020-11-03 12:06:43 +0530

    Add files via upload
    Java program

---
+++ Employee.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Employee{
+
+public static void main(String[] args){
+
+Scanner sc=new Scanner(System.in);
+String s="",add="";
+int year;
+float salary;
+System.out.println("Name    Year of joining   Salary   Address  ");
+for(int i=0;i<3;i++){
+
+
+System.out.println(sc.next()+"        "+sc.nextInt()+"                "+sc.nextInt()+"                  "+sc.next());
+}
+}
+} 
